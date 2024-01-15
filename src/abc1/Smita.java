1e7613398459d221251023d9aa4ad4450083f3df
PrashantRonge <prashantronge555@gmail.com>
2024-01-15 15:39:16 +0530

    [p]:hi

---
+++ src/abc1/Smita.java
@@ -0,0 +1,12 @@
+package abc1;
+
+public class Smita {
+
+	public static void main(String[] args) {
+		System.out.println("first update new ");
+		System.out.println("Second update new");
+		System.out.println("Second update new1");
+
+	}
+
+}
