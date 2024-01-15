e50554b4cb827cfa4cd1fe9e7a42d5427280f1d5
PrashantRonge <prashantronge555@gmail.com>
2024-01-15 15:14:12 +0530

    [Prashant]: new update

---
+++ src/ABC1/Class1.java
@@ -0,0 +1,11 @@
+package ABC1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		System.out.println("first update new ");
+		System.out.println("Second update new");
+
+	}
+
+}
